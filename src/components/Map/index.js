7e17c68cb9a465e7807823a423f245bcd6ed1422
Afonso Machado <afonso777@hotmail.com>
2020-08-31 23:36:56 -0300

    exibição do mapa na tela4

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,20 +1,49 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
 import MapView from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
 
-export default class index extends Component {
+export default class Map extends Component {
+  state = {
+    region: null,
+  };
+
+  // executado quando o componente for renderizado em tela
+  async componentDidMount() {
+    Geolocation.getCurrentPosition(
+      ({coords: {latitude, longitude}}) => {
+        //da erro aqui
+        console.log('LATITUDE', latitude);
+        this.setState({
+          region: {
+            latitude,
+            longitude,
+            latitudeDelta: 0.0143,
+            longitudeDelta: 0.0134,
+          },
+        });
+      }, //sucesso
+      () => {}, //erro
+      {
+        // tempo buscando a localização até dar erro
+        timeout: 2000,
+        // pegar localização via gps
+        enableHighAccuracy: true,
+        // cache da informação do usuario, para no maximo 1 segundo
+        maximumAge: 1000,
+      },
+    );
+  }
+
   render() {
+    const {region} = this.state;
+    // console.log(region);
+
     return (
       <View style={{flex: 1}}>
         <MapView
           style={{flex: 1}}
-          region={{
-            latitude: -27.210753,
-            longitude: -49.644173,
-            // zoom
-            latitudeDelta: 0.0143,
-            longitudeDelta: 0.0134,
-          }}
+          region={region}
           showsUserLocation
           loadingEnabled
         />
